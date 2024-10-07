13b58f105fcc8124a4c05df5ff3b7918f5e49e67
Mostafa918M <Mostafa918M>
2024-10-07 22:28:54 +0300

    THIS IS THE CALCULATER

---
+++ src/main/java/com/mycompany/gui/GUI.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gui;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Mostafa
+ */
+public class GUI {
+
+    public static void main(String[] args) {
+        new NewJFrame().setVisible(true);
+        
+    }
+}
